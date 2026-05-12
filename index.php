c0c05a892b5d1db0e28c1ab2a8ead3000e14f71e
MayaTallbom <maya@tallbom.se>
2026-05-12 10:18:26 +0200

    Bytt till php, börjat skapa login

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+    session_start();
+    require 'config.php';
+
+    $error = "";
+      
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+          
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+          
+        $stmt = $pdo->prepare("SELECT * FROM User WHERE Username = :username");
+        $stmt->bindParam(':username', $username);
+        $stmt->execute();
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+          
+        if ($user && password_verify($password, $user['Password'])) {
+          $_SESSION['user_id'] = $user['Id'];
+          $_SESSION['username'] = $user['Username'];
+          header("Location: index.php");
+          exit;
+          } else {
+            $error = "Invalid username or password.";
+          }
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styleLogin.css">
+</head>
+<body>
+    <header>
+        <h1>Eventutskottet</h1>
+        <a href="index.php">Hem</a>
+    </header>
+    <div class="center-flex" id="container">
+		<form action="login.php" method="post">
+        <?php if ($error != ""): ?>
+            <p><?php echo $error; ?></p>
+        <?php endif; ?>
+			<label for="username">Username:</label>
+			<input type="text" name="username" id="username" placeholder="Username" required minlength="4">
+			<label for="password">Password:</label>
+			<input type="password" name="password" id="password" placeholder="Password" pattern="(?=.*\d)(?=.*[a-zåäö])(?=.*[A-ZÅÄÖ]).{8,}" required title="Must contain at least one numeric value, one lowercase letter and one uppercase letter">
+			<input type="submit" value="Log in">
+            Don't Have An Account? <a href="createAcc.php">Sign up here!</a>
+		</form>
+    </div>
+</body>
+</html>
