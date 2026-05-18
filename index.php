a6dde080249cfbac3f75b3094dfcd0133d70ed03
MayaTallbom <maya@tallbom.se>
2026-05-18 11:04:28 +0200

    Kämpar med admin och db

--- index.php
+++ index.php
@@ -15,7 +15,7 @@
         $user = $stmt->fetch(PDO::FETCH_ASSOC);
           
         if ($user && password_verify($password, $user['password'])) {
-          $_SESSION['user_id'] = $user['id'];
+          $_SESSION['user_id'] = $user['userID'];
           $_SESSION['username'] = $user['username'];
           $_SESSION['role'] = $user['role'];
           header("Location: mainPage.php");
