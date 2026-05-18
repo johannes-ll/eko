f13980b3faa24fd90e29c47acc782186f27281d2
johannes <45330378+johannes-ll@users.noreply.github.com>
2026-05-18 12:18:56 +0200

    hjälp mig

---
+++ getEvents.php
@@ -0,0 +1,9 @@
+<?php>
+require 'config.php'
+$query = "SELECT * FROM Event";
+$content = $pdo->prepare($query);
+$content->execute();
+while($row = $content->fetch(PDO::FETCH_ASSOC)) {
+    echo $row['title'] . " " . $row['info'] . " " . $row['date'] . " " . $row['time'] . " " . $row['longitude'] . " " . $row['latitude'] . "<br>";
+}
+</?php>
