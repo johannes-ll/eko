2b7df98830f8946ee887c9acb8b2aa3a7ceae3ee
MayaTallbom <maya@tallbom.se>
2026-05-22 16:27:15 +0200

    Förlåt jag krasha hela sidan

--- loggedInCheck.php
+++ loggedInCheck.php
@@ -1,5 +1,5 @@
 <?php
-/*Denna fil används för att kontrollera om en användare är inloggad. Den startar en session och kontrollerar om 'user_id' är satt i sessionen.
+/*Denna fil används för att kontrollera om en användare är inloggad. Den startar en session och kontrollerar om 'user_id' är satt i sessionen.*/
 if (!isset($_SESSION['user_id'])) {
     header("Location: index.php");
     exit;
