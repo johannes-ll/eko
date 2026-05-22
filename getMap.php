f72cf804ed6ee39894567cfc96c6d1ce525808f6
Silip34 <70316264+Imorax@users.noreply.github.com>
2026-05-22 10:45:15 +0200

    Update getMap.php

--- getMap.php
+++ getMap.php
@@ -1,60 +1,83 @@
-<?php
-// ändra
-$latitude = 59.8579;
-$longitude = 17.6392;
-$zoomLevel = 15;
-?>
-
-
 <!DOCTYPE html>
-<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>OpenStreetMap</title>
+    <meta name="viewport" id="vp" content="initial-scale=1.0,user-scalable=no,maximum-scale=1,width=device-width" />
+    <meta charset="utf-8" />
 
 
-    <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
+    <link rel="stylesheet" href="https://api.mazemap.com/js/v2.2.6/mazemap.min.css">
+    <script type='text/javascript' src='https://api.mazemap.com/js/v2.2.6/mazemap.min.js'></script>
+
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 20px;
-        }
-        #map {
-            height: 500px;
-            width: 100%;
-            border-radius: 8px;
-        }
+        body { margin:0px; padding:0px; width: 100vw; height:100vh; }
+        #map { width: 100%; height: 100%; }
     </style>
 </head>
 <body>
+    <div id="map" class="mazemap"></div>
 
-
-    <div id="map"></div>
-
-
-    <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
-   
     <script>
-        // ändra till let för låta ändra senare
-        const lat = <?php echo $latitude; ?>;
-        const lon = <?php echo $longitude; ?>;
-        const zoom = <?php echo $zoomLevel; ?>;
 
+      const startLng = 17.619748;
+      const startLat = 59.859456;
+      const startZ = 1;
 
-        const map = L.map('map').setView([lat, lon], zoom);
+        var map = new Mazemap.Map({
+            container: 'map',
+            campuses: 110,
+            center: {lng: startLng, lat: startLat},
+            zoom: 17,
+            zLevel: startZ
+        });
 
+        var routeDrawer;
+        var currentPopup;
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+        map.on('load', function() {
+          routeDrawer = new Mazemap.AtoBTripBasicDrawer(map, {
+            routeLineColorPrimary: '#0099EA', 
+            showDirectionArrows: true
+          });
 
+          map.on('click', onMapClick);
+        });
 
-        const marker = L.marker([lat, lon]).addTo(map);
-        marker.bindPopup("<b>You are here</b>").openPopup();
+        function onMapClick(e) {
+          var targetLngLat = e.lngLat;
+          var targetZ = map.zLevel;
+
+          if(currentPopup) {
+            currentPopup.remove();
+          }
+
+          if(routeDrawer) {
+            routeDrawer.clear();
+          }
+
+            var fromString = `${startLng},${startLat},${startZ}`;
+            var toString = `${targetLngLat.lng},${targetLngLat.lat},${targetZ}`;
+
+            const routeParams = {
+              mode: "PEDESTRIAN",
+              campusCollectionTag: "uu",
+              fromLngLatZ: fromString,
+              toLngLatZ: toString,
+              lang: "sv"
+            };
+
+            Mazemap.Data.getAtoBTrip(routeParams)
+            .then(function(trip) {
+              routeDrawer.setAtoBTrip(trip);
+
+            Mazemap.Data.getPoiAt(targetLngLat, targetZ)
+            .then(poi => {
+            var poiName = (poi && poi.properties && poi.properties.name) ? poi.properties.name : "Framme!";
+
+              currentPopup = new Mazemap.Popup()
+              .setLngLat(targetLngLat)
+              .setHTML(`${poiName}`)
+              .addTo(map);
+            });
+        });
+        }
     </script>
-
-
 </body>
-</html>
